e8c718303fb0a14cab85519d0d93f22a88254f80
Richa Sharma <richasharmaupw@gmail.com>
2019-09-27 13:22:41 +0530

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -58,4 +59,23 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  # config.action_mailer.delivery_method = :testq2
+  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+  config.action_mailer.delivery_method =:smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    authentication: 'plain',
+    # enable_starttls_auto: true,
+    user_name: '',
+    password: '',
+    domain: 'gmail.com',
+  }
+  # Use this if developing on localhost.
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 end
