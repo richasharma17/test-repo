2354d666e06ca65f8f94e5a35677324d1c178cb0
Richa Sharma <richasharmaupw@gmail.com>
2019-10-03 23:55:08 +0530

    Add user following

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,9 +10,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	get signup_path
   	assert_no_difference 'User.count' do
   		post users_path, params: { user: { name: "",
-  									       email: "user@invalid",
-                                           password: "foo",
-                                           password_confirmation: "bar" }}
+  									                     email: "user@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar" }}
 	end
 	assert_template 'users/new'  
   end
